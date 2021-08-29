ca0e3c6c19d672a21ea9ac7a52b5aead4d69fbf3
bastien <b@kuma.la>
2021-08-29 11:10:02 +0400

    Improve the interface architecture

---
+++ stage2/04-pirate-radio/files/usr/local/lib/radio-interface/controls.py
@@ -0,0 +1,141 @@
+"""The class that implements the controls for the audio device."""
+import asyncio
+from contextlib import contextmanager
+from subprocess import run
+
+import alsaaudio
+import pulsectl
+import simpleaudio as sa
+from gpiozero import MCP3008, Button
+from mpd import MPDClient
+
+VOLUME_STEP = 0.1
+SLEEP_TIMER_SOUND = "/usr/local/lib/radio-interface/sleep-timer.wav"
+HOST, PORT = "localhost", 6600
+POTENTIOMETER_THRESHOLD_TRIGGER = 0.01
+# use the trick described here:
+# https://gpiozero.readthedocs.io/en/stable/faq.html
+# #how-do-i-use-button-when-pressed-and-button-when-held-together
+Button.was_held = False
+
+
+class Controls:
+    """Implements the various controls needed in an audio device.
+
+    mpd client, pulse client and potentiometer_volume are class variable
+    because these resources are better to be shared by all instances of
+    the Controls class. (Even though a typical use case won't require
+    more than 1 instance of the class.)
+    """
+
+    mpd = MPDClient()
+    pulse_client = pulsectl.Pulse("volume")
+    potentiometer_volume = MCP3008(0)
+
+    def __init__(self) -> None:
+        self.button_was_held: bool = False
+        self.pulse_sink = self.pulse_client.sink_list()[0]
+
+    @contextmanager
+    def connection_to_mpd(self):
+        """Context manager to establish the connection with MPD.
+
+        Should be used for every use of the client since the connection is
+        sketchy.
+        """
+        try:
+            self.mpd.connect(HOST, PORT)
+            yield
+        finally:
+            self.mpd.close()
+            self.mpd.disconnect()
+
+    def volume_down(self) -> None:
+        """Volume down button tells pulseaudio to step down the volume."""
+        self.pulse_client.volume_change_all_chans(self.pulse_sink, -VOLUME_STEP)
+
+    def volume_up(self) -> None:
+        """Volume up button tells pulseaudio to step up the volume."""
+        self.pulse_client.volume_change_all_chans(self.pulse_sink, +VOLUME_STEP)
+
+    async def volume_knob(self) -> None:
+        """
+        Set the volume according to the potentiometer.
+        Gpiozero returns a float from 0 to 1 from the potentiometer.
+        It can be fed directly into pulse audio, 1 being the maximum
+        volume (above 1 is the soft boost).
+
+        The potentiometer has an electrical charge that varies
+        enough to constantly trigger a volume change.
+        We make sure we only trigger the volume change when the
+        volume knob is actually moved.
+        That also helps in the use case when the volume is set by
+        other means, like the web interface, it avoids the
+        situation where the volume knob would cancel the volume
+        change by constantly setting it back to its own level.
+
+        The potentiometer value moves roughly by more or less 0.01.
+        """
+        comparison_point = self.potentiometer_volume.value
+        while True:
+            if (
+                abs(self.potentiometer_volume.value - comparison_point)
+                > POTENTIOMETER_THRESHOLD_TRIGGER
+            ):
+                volume = self.potentiometer_volume.value
+                self.pulse_client.volume_set_all_chans(self.pulse_sink, volume)
+            comparison_point = self.potentiometer_volume.value
+            await asyncio.sleep(0.1)
+
+    def play_pause(self) -> None:
+        """Play/pause button tells MPD to toggle play/plause."""
+        with self.connection_to_mpd():
+            self.mpd.pause()
+
+    def next(self) -> None:
+        """Next button tells MPD to play next track."""
+        with self.connection_to_mpd():
+            self.mpd.next()
+
+    def previous(self) -> None:
+        """Previous button tells MPD to play previous track."""
+        with self.connection_to_mpd():
+            self.mpd.previous()
+
+    def sleep_timer(self, button) -> None:
+        """Shutdown button tells the system to shutdown 20 minutes from now."""
+        self.button_was_held = True
+        command = """shutdown
+                   -h +20
+                   """
+        run(command.split())
+        command = """wall
+                   -n Sleep timer was triggered. System is shutting down in 20
+                   minutes.
+                   """
+        run(command.split())
+        self._notify()
+
+    def _notify(self) -> None:
+        """
+        Play a sound to inform the user that the long button press is
+        registered.
+        """
+        sleep_timer_sound = sa.WaveObject.from_wave_file(SLEEP_TIMER_SOUND)
+        notification = sleep_timer_sound.play()
+        notification.wait_done()
+
+    def shutdown(self, button) -> None:
+        """Shutdown button tells the system to shutdown now."""
+        if not self.button_was_held:
+            with self.connection_to_mpd():
+                self.mpd.stop()
+            command = """shutdown
+                    -h now
+                    """
+            run(command.split())
+            command = """wall
+                        -n Power off was triggered by user.
+                       """
+            run(command.split())
+        self.button_was_held = False
---
+++ stage2/04-pirate-radio/files/usr/local/lib/radio-interface/interface.py
@@ -0,0 +1,63 @@
+"""This module is a systemd service that takes care of interfacing the
+physical interaction devices (buttons, LEDs...) and the controls of the
+radio.
+"""
+import asyncio
+from signal import pause
+
+import systemd.daemon
+from controls import Controls
+from gpiozero import Button
+from mpd.base import CommandError
+
+controls = Controls()
+# if the playlist is empty and an .m3u file has been provided then initialize
+with controls.connection_to_mpd():
+    if not controls.mpd.playlist():
+        try:
+            controls.mpd.load("my-playlist")
+        except (CommandError):
+            pass
+
+# Make sure the client is playing and in repeat mode on startup
+with controls.connection_to_mpd():
+    controls.mpd.play()
+    controls.mpd.repeat(1)
+
+# all initialization is considered done after this point and we tell
+# systemd that we are ready to serve
+systemd.daemon.notify("READY=1")
+
+# Retain compatibility with phat-beat buttons
+phatbeat_fast_forward_gpio = 5
+phatbeat_rewind_gpio = 13
+phatbeat_play_pause_gpio = 6
+phatbeat_volume_up_gpio = 16
+phatbeat_volume_down_gpio = 26
+phatbeat_on_off_button = Button(12)
+
+# Create the buttons by assigning their corresponding GPIOs
+fast_forward_button = Button(phatbeat_fast_forward_gpio)
+rewind_button = Button(phatbeat_rewind_gpio)
+play_pause_button = Button(phatbeat_play_pause_gpio)
+volume_up_button = Button(phatbeat_volume_up_gpio)
+volume_down_button = Button(phatbeat_volume_down_gpio)
+on_off_button = Button(17)
+
+# Define what actions to set for each button event
+fast_forward_button.when_pressed = controls.next
+rewind_button.when_pressed = controls.previous
+play_pause_button.when_pressed = controls.play_pause
+volume_up_button.when_pressed = controls.volume_up
+volume_down_button.when_pressed = controls.volume_down
+on_off_button.when_held = controls.sleep_timer
+on_off_button.when_released = controls.shutdown
+phatbeat_on_off_button.when_held = controls.sleep_timer
+phatbeat_on_off_button.when_released = controls.shutdown
+
+# sync the volume with the potentiometer value
+asyncio.run(controls.volume_knob())
+
+# maintain the module loaded for as long the the interface is needed
+# without consuming resources
+pause()
